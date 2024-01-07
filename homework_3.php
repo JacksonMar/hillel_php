82ca7737693881a15f5185ac0ea9570e010f897b
Евгений !!! <0663132300@ukr.net>
2024-01-07 18:33:10 +0100

    refactoring code

--- homework_3.php
+++ homework_3.php
@@ -2,15 +2,14 @@
 
 echo "Hello, enter number from 1 to 6 : ";
 $valueNumber = trim(fgets(STDIN));
-$value = (int)$valueNumber;
 
-echo match ($value) {
-    1 => "The number {$value} is displayed in green colour. " . PHP_EOL,
-    2 => "The number {$value} is displayed in red colour. " . PHP_EOL,
-    3 => "The number {$value} is displayed in blue colour. " . PHP_EOL,
-    4 => "The number {$value} is displayed in brown colour. " . PHP_EOL,
-    5 => "The number {$value} is displayed in violet colour. " . PHP_EOL,
-    6 => "The number {$value} is displayed in black colour. " . PHP_EOL,
+echo match ($valueNumber) {
+    "1" => "The number {$valueNumber} is displayed in green colour. " . PHP_EOL,
+    "2" => "The number {$valueNumber} is displayed in red colour. " . PHP_EOL,
+    "3" => "The number {$valueNumber} is displayed in blue colour. " . PHP_EOL,
+    "4" => "The number {$valueNumber} is displayed in brown colour. " . PHP_EOL,
+    "5" => "The number {$valueNumber} is displayed in violet colour. " . PHP_EOL,
+    "6" => "The number {$valueNumber} is displayed in black colour. " . PHP_EOL,
     default => "You entered '{$valueNumber}', and it's not clear, so your color is white !" . PHP_EOL
 };
 
