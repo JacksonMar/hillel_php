29ede75294a881afd07300ffc202f13883c2f1d7
Евгений !!! <0663132300@ukr.net>
2024-02-20 14:05:39 +0100

    homework-20

--- homework_20.php
+++ homework_20.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Точка входу</title>
 </head>
 <body>
-<p>Введіть два числа для складання:</p>
 <form method="post" action="/ht_20_response.php">
+    <p>Введіть два числа для складання:</p>
     <label for="number1">Число 1:</label>
     <input type="text" id="number1" name="number1"><br><br>
     <label for="number2">Число 2:</label>
